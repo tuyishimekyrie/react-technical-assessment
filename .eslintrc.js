11517342937dc432501160e58c989a597cfa288e
tuyishimekyrie <tuyishimehope01@gmail.com>
2024-10-31 14:24:01 +0200

    removed descrepencaies in packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,10 @@ module.exports = {
     },
     extends: [
       'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
     ],
-    parser: '@typescript-eslint/parser',
     parserOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
-    },
-    rules: {
-      'constructor-super': 'off', // Disable if causing issues
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   };
   
